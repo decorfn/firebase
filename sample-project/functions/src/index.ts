bf69c1a79bc50d9f4ba46c20c764035a566426a6
Michael Bats <michael@batstech.com>
2019-07-08 09:55:44 +0100

    0.0.3

--- sample-project/functions/src/index.ts
+++ sample-project/functions/src/index.ts
@@ -1,61 +1,55 @@
-import * as functions from 'firebase-functions';
-import { Controller, Main } from '../../../lib/src/Main';
-import {
-    ClassCallMiddleware,
-    CallMiddleware,
-    NextFunction
-} from '../../../lib/src/decorators';
+// import * as functions from 'firebase-functions';
 
-const callMid1: CallMiddleware = (
-    data: any,
-    context: functions.https.CallableContext,
-    next: NextFunction
-) => {
-    console.log('CALL MID 1');
-    data.callMid1 = true;
-    next();
-};
+// const callMid1: CallMiddleware = (
+// 	data: any,
+// 	context: functions.https.CallableContext,
+// 	next: NextFunction
+// ) => {
+// 	console.log('CALL MID 1');
+// 	data.callMid1 = true;
+// 	next();
+// };
 
-const callMid2: CallMiddleware = (
-    data: any,
-    context: functions.https.CallableContext,
-    next: NextFunction
-) => {
-    console.log('CALL MID 2');
-    data.callMid2 = true;
-    next();
-};
+// const callMid2: CallMiddleware = (
+// 	data: any,
+// 	context: functions.https.CallableContext,
+// 	next: NextFunction
+// ) => {
+// 	console.log('CALL MID 2');
+// 	data.callMid2 = true;
+// 	next();
+// };
 
-const callMid3: CallMiddleware = (
-    data: any,
-    context: functions.https.CallableContext,
-    next: NextFunction
-) => {
-    console.log('CALL MID 3');
-    data.callMid3 = true;
-    next();
-};
+// const callMid3: CallMiddleware = (
+// 	data: any,
+// 	context: functions.https.CallableContext,
+// 	next: NextFunction
+// ) => {
+// 	console.log('CALL MID 3');
+// 	data.callMid3 = true;
+// 	next();
+// };
 
-@ClassCallMiddleware([callMid1])
-export class TestController implements Controller {
-    @CallMiddleware([callMid2, callMid3])
-    TestMethod(data: any, context: functions.https.CallableContext) {
-        console.log(data);
-        console.log('THIS IS THE ACUTAL METHOD');
-    }
-}
+// @ClassCallMiddleware([callMid1])
+// export class TestController implements Controller {
+// 	@CallMiddleware([callMid2, callMid3])
+// 	TestMethod(data: any, context: functions.https.CallableContext) {
+// 		console.log(data);
+// 		console.log('THIS IS THE ACUTAL METHOD');
+// 	}
+// }
 
-export class TestMain extends Main {
-    theTest = new TestController();
-    constructor() {
-        super();
-        this.controllers = [this.theTest];
-    }
+// export class TestMain extends Main {
+// 	theTest = new TestController();
+// 	constructor() {
+// 		super();
+// 		this.controllers = [this.theTest];
+// 	}
 
-    run() {
-        this.start();
-    }
-}
+// 	run() {
+// 		this.start();
+// 	}
+// }
 
-let functionsApp = new TestMain();
-functionsApp.run();
+// let functionsApp = new TestMain();
+// functionsApp.run();
--- src/Main.ts
+++ src/Main.ts
@@ -4,78 +4,58 @@ import * as functions from 'firebase-functions';
 import { ClassKeys } from './decorators';
 
 interface FunctionsIndex {
-    [key: string]: any;
+	[key: string]: any;
 }
 
 export class Controller implements FunctionsIndex {
-    [key: string]: any;
+	[key: string]: any;
 }
 
 export class Main {
-    _controllers: Controller[] = [];
-    public set controllers(controllers: Controller[]) {
-        this._controllers = controllers;
-    }
-    // calling it in index.ts where it will fill the file with the functions
-    protected start() {
-        // iterating over all controllers/classes
-        for (let controller of this._controllers) {
-            // making sure we're only passing our Controller class through an iterator;
-            let cPrototype = Object.getPrototypeOf(controller);
+	_controllers: Controller[] = [];
+	public set controllers(controllers: Controller[]) {
+		this._controllers = controllers;
+	}
+	// calling it in index.ts where it will fill the file with the functions
+	protected start() {
+		// iterating over all controllers/classes
+		for (let controller of this._controllers) {
+			// making sure we're only passing our Controller class through an iterator;
+			let classProto = Object.getPrototypeOf(controller);
+			let cMiddle = Reflect.getMetadata(ClassKeys.CallMiddleware, classProto);
+			for (let fn in controller) {
+				let functionProto = Object.getPrototypeOf(controller[fn]);
+				let fnOptions = Reflect.getMetadata(ClassKeys.Options, functionProto);
+				let fnName = Reflect.getMetadata('fnName', functionProto);
+				let fnMiddleware = Reflect.getMetadata(ClassKeys.CallMiddleware, functionProto);
 
-            for (let fn in controller) {
-                let fnPrototype = Object.getPrototypeOf(controller[fn]);
-                console.log(fnPrototype);
-                console.log(cPrototype);
+				if (fnOptions.type === 'onCall') {
+					exports[controller.constructor.name + '_' + fnName] = functions
+						.region(fnOptions.region)
+						.https.onCall((data: any, context: functions.https.CallableContext) => {
+							let middleware = [...cMiddle, ...fnMiddleware];
+							let done = 0;
+							next();
+							function next(err?: functions.https.HttpsError | any) {
+								// error handling
+								if (err) {
+									if (err instanceof functions.https.HttpsError) {
+										throw err;
+									} else {
+										throw new functions.https.HttpsError('unknown', 'An unknown error has occured.');
+									}
+								}
 
-                if (fnPrototype.options.type === 'onCall') {
-                    exports[
-                        controller.constructor.name + '_' + fnPrototype.fnName
-                    ] = functions
-                        .region(fnPrototype.options.region)
-                        .https.onCall(
-                            (
-                                data: any,
-                                context: functions.https.CallableContext
-                            ) => {
-                                let middleware = [
-                                    ...cPrototype[ClassKeys.CallMiddleware],
-                                    ...fnPrototype[ClassKeys.CallMiddleware]
-                                ];
-                                let done = 0;
-                                next();
-                                function next(
-                                    err?: functions.https.HttpsError | any
-                                ) {
-                                    // error handling
-                                    if (err) {
-                                        if (
-                                            err instanceof
-                                            functions.https.HttpsError
-                                        ) {
-                                            throw err;
-                                        } else {
-                                            throw new functions.https.HttpsError(
-                                                'unknown',
-                                                'An unknown error has occured.'
-                                            );
-                                        }
-                                    }
-
-                                    if (done === middleware.length) {
-                                        return controller[fnPrototype.fnName](
-                                            data,
-                                            context
-                                        );
-                                    } else {
-                                        middleware[done](data, context);
-                                        done++;
-                                    }
-                                }
-                            }
-                        );
-                }
-            }
-        }
-    }
+								if (done === middleware.length) {
+									return controller[fnName](data, context);
+								} else {
+									middleware[done](data, context);
+									done++;
+								}
+							}
+						});
+				}
+			}
+		}
+	}
 }
